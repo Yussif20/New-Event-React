bcab962ddfe7c8a724e195a42c5c3455c694472f
Yousef <yousefayman2000500@gmail.com>
2022-11-02 21:05:35 +0200

    adding the speaker pictures

--- src/design-system/components/OverviewBar/OverviewBar-component.jsx
+++ src/design-system/components/OverviewBar/OverviewBar-component.jsx
@@ -1,6 +1,6 @@
 import OverviewItem from "../OveviewItem/OverviewItem-component";
 import { OverviewWrapper } from "./OverviewBar-styles";
-import OverviewData from "../../../OverviewData";
+import OverviewData from "./OverviewData";
 
 export default function OverviewBar() {
   return (
