2ea7d02c976987c401580a9a688c4f004398667c
Yousef <yousefayman2000500@gmail.com>
2022-11-06 23:26:37 +0200

    making our sponsors data

---
+++ src/design-system/components/OurSponsors/OurSponsors-component.jsx
@@ -0,0 +1,3 @@
+export default function OurSponsors() {
+  return <div>{}</div>;
+}
---
+++ src/design-system/components/OurSponsors/OurSponsorsData.jsx
@@ -0,0 +1,34 @@
+import img1 from "../../../shared/assets/logos/business-insider.png";
+import img2 from "../../../shared/assets/logos/forbes.png";
+import img3 from "../../../shared/assets/logos/techcrunch.png";
+import img4 from "../../../shared/assets/logos/the-new-york-times.png";
+import img5 from "../../../shared/assets/logos/usa-today.png";
+
+const OurSponsorsData = [
+  {
+    id: "1",
+    alt: "sponsor",
+    img: img1,
+  },
+  {
+    id: "2",
+    alt: "sponsor",
+    img: img2,
+  },
+  {
+    id: "3",
+    alt: "sponsor",
+    img: img3,
+  },
+  {
+    id: "4",
+    alt: "sponsor",
+    img: img4,
+  },
+  {
+    id: "5",
+    alt: "sponsor",
+    img: img5,
+  },
+];
+export default OurSponsorsData;
